48a7ed227d0e462e0d1dcc5f2d0f5e69ea0d3101
yuandaxing <mryuan0@gmail.com>
2016-04-14 17:27:14 +0800

    update c11

--- talk-2016-04-14/compile.sh
+++ talk-2016-04-14/compile.sh
@@ -2,4 +2,4 @@
 
 g++  -O3 -o binary_search_ut.exe binary_search_ut.cpp -std=c++11 -I../../lib/gtest -L../../lib/gtest -fdiagnostics-color=auto -lgtest -lpthread
 
-/usr/bin/g++  -O3 -o binary_search-test1.exe binary_search-test1.cpp -I../../lib/gtest -L../../lib/gtest  -lgtest -lpthread
+/usr/bin/g++  -O3 -o binary_search-test1.exe binary_search-test1.cpp -std=c++11 -I../../lib/gtest -L../../lib/gtest  -lgtest -lpthread
