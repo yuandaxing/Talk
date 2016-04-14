d73d0989ac0747f252c1413cc4cabd79fece7747
yuandaxing <mryuan0@gmail.com>
2016-04-14 17:21:38 +0800

    update compile

--- talk-2016-04-14/compile.sh
+++ talk-2016-04-14/compile.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-g++  -O3 -o thread_manager_ut.exe thread_manager_ut.cpp -std=c++11 -I../../lib/gtest -L../../lib/gtest -fdiagnostics-color=auto -lgtest -lpthread
+g++  -O3 -o binary_search_ut.exe binary_search_ut.cpp -std=c++11 -I../../lib/gtest -L../../lib/gtest -fdiagnostics-color=auto -lgtest -lpthread
 
-/usr/bin/g++  -O3 -o old.exe old.cpp -I../../lib/gtest -L../../lib/gtest  -lgtest -lpthread
+/usr/bin/g++  -O3 -o binary_search-test1.exe binary_search-test1.cpp -I../../lib/gtest -L../../lib/gtest  -lgtest -lpthread
